7cda0b5a4ae72ca390bbef988991168e5754de14
kevjose <kevin.jose@letsventure.com>
2016-12-24 10:14:08 +0530

    Project Setup: Render react component

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 let app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.listen(3000, ()=> console.log("Localhost running at port:3000"));
+app.listen(3000, () => console.log("Localhost running at port:3000"));
