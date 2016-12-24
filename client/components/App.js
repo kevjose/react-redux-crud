468cfb70cc4d349817736024ef7a1f1f3a573b2b
kevjose <kevin.jose@letsventure.com>
2016-12-24 11:30:02 +0530

    Project setup: Hot reloading setup

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Greetings from './Greetings';
 
-export default () => {
-  return (
-    <h1>Hello From React</h1>
-  );
+// export default () => {
+//   return (
+//     <h1>Hello From React!</h1>
+//   );
+// }
+
+class App extends React.Component {
+  render(){
+    return (
+      <Greetings />
+    );
+  }
 }
+export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,26 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
-   entry: path.join(__dirname, '/client/index.js'),
+   entry:[
+     'webpack-hot-middleware/client',
+     path.join(__dirname, '/client/index.js')
+   ],
    output: {
-     path: '/'
+     path: '/',
+     publicPath: '/'
    },
+   plugins: [
+     new webpack.NoErrorsPlugin(),
+     new webpack.optimize.OccurenceOrderPlugin(),
+     new webpack.HotModuleReplacementPlugin()
+   ],
    module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel' ]
+        loaders: ['react-hot', 'babel' ]
       }
     ]
   },
